cc8815a854083b3edc23161f14f012abbaf0a4df
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-02-05 00:08:07 +0000

    new chnges

--- src/main/java/ArrayConcept/SortingElimentsinArray.java
+++ src/main/java/ArrayConcept/SortingElimentsinArray.java
@@ -6,12 +6,17 @@ public class SortingElimentsinArray {
     public static void main(String args[]){
         int a[]={200,500,600,100,400};
         System.out.println("before sorting");
-        System.out.println(Arrays.toString(a));
 
+       /* for (int value: a){
+            System.out.println(value);
+        }*/
 
-        System.out.println("After sorting");
+        // just print array using predifine methos
+        System.out.println(Arrays.toString(a)); // print array without for loop
+
+       System.out.println("After sorting");
         Arrays.sort(a);
-        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(a)); 
 
     }
 }
