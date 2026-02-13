47109858bdb82c7d729ab8b60febe89292da2bc4
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-02-13 23:49:19 +0000

    new Changes

---
+++ src/main/java/StringConcept/StringMethods.java
@@ -0,0 +1,13 @@
+package StringConcept;
+
+public class StringMethods {
+    public static void main(String args[]){
+        String s= "Welcome";
+        String n= new String("Welcome");
+        System.out.println(s);
+        // lenght method returns lenght of thesting number of charactors
+        int strlenght = s.length();
+        System.out.println(strlenght);
+
+    }
+}
