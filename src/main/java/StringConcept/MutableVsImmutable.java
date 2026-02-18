a6dae65bcbde629f13416de9a2f4c3e1fa1b9e87
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-02-18 15:29:53 +0000

    new Changes

--- src/main/java/StringConcept/MutableVsImmutable.java
+++ src/main/java/StringConcept/MutableVsImmutable.java
@@ -1,8 +1,19 @@
 package StringConcept;
 
+import java.util.Arrays;
+
 public class MutableVsImmutable {
     public static void main(String args[]){
 
-        
+        // What is mutability and immutable
+        // mutable means we can change
+        // immutable means we can not change
+        // String is immutable and String Buffer and Builder is mutable
+
+        // Example of Mutable
+        int a[]= {30,40,50,20,10};
+        System.out.println("Before sorting the array" + Arrays.toString(a));
+        Arrays.sort(a);
+        System.out.println("After sorting the Array" + Arrays.toString(a));
     }
 }
